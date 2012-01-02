c1a99a26401702a653d1ed5c4c624ed770d4da50
Adam Hitchcock <adam@northisup.com>
2012-01-01 21:36:47 -0800

    cleanup

--- memeer.py
+++ memeer.py
@@ -1,29 +1,48 @@
 import gevent.monkey
 gevent.monkey.patch_all()
 
-from urllib import quote
+## application
+import libmeme
+from template import THREAD
+from  content_types_mixin import IMAGE
 
 ## make wsgi easier
 from gserver.routes import Routes
 from gserver.wsgi import WSGIServer
 
-# utils
+## utils
+from urllib import quote
 from paver.path import path
 import cStringIO
 
-# debug
-import libmeme
+from logging import getLogger
 
-from content_types import IMAGE
+LOG = getLogger(__name__)
 
 routes = Routes()
 route = routes.route
 route_json = routes.route_json
 
 
-@route("^/example/$")
-def example(req):
-        return "hello"
+@route("^/memeer/disqus/(?P<name>[\w\s_\-]+)/(?P<line_a>[\w\s_\-]+)/(?P<line_b>[\w\s_\-]+)[/]?$")
+def serve_meme_thread(req, name, line_a, line_b):
+    _meme_img, better_name = libmeme.meme_image(name, line_a, line_b)
+
+    line_a_format = quote(line_a)
+    line_b_format = quote(line_b)
+
+    final_name = better_name if better_name != name else name
+
+    meme_base = "/{0}/{1}/{2}/".format(final_name, line_a_format, line_b_format)
+    meme_link = "/memeer" + meme_base
+    disqus_link = "/memeer/disqus" + meme_base
+
+    if better_name != name:
+        LOG.info("redirecting to: ", disqus_link)
+        req.redirect(disqus_link)
+
+    ret = (THREAD.format(meme_link=meme_link, meme_name=final_name, line_a=line_a, line_b=line_b))
+    return ret
 
 
 @route("^/memeer/(?P<name>[\w\s_\-]+)/(?P<line_a>[\w\s_\-]+)/(?P<line_b>[\w\s_\-]+)[/]?$", content_type=IMAGE.JPEG)
@@ -34,6 +53,7 @@ def serve_meme(req, name, line_a, line_b):
         line_a_format = quote(line_a)
         line_b_format = quote(line_b)
         new_url = "/memeer/{0}/{1}/{2}/".format(better_name, line_a_format, line_b_format)
+        LOG.info("redirecting to: ", new_url)
         req.redirect(new_url)
 
     f = cStringIO.StringIO()
@@ -48,6 +68,6 @@ if __name__ == '__main__':
     meme_path = path("memes").abspath()
     libmeme.populate_map(meme_path)
     port = 8088
-    print 'Serving on %d...' % port
+    LOG.info('Serving on %d...' % port)
     server = WSGIServer(('', port), routes)
     server.serve_forever()
